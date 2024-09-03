64e6e2740d0561868989f88080ba1460c4a8a6a1
Tommaso1699 <69764736+Tommaso1699@users.noreply.github.com>
2024-09-03 12:30:24 +0200

    Added functionality to mark task as done and delete it

--- Task_List/src/app/app.component.ts
+++ Task_List/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,18 +14,27 @@ export class AppComponent {
   title = 'Task_List';
   taskTitle: string = '';
   taskDescription: string = '';
-  tasks: { title: string, description: string }[] = [];
+  tasks: { title: string; description: string; done: boolean }[] = [];
 
   onSubmit() {
     if (this.taskTitle && this.taskDescription) {
       this.tasks.push({
         title: this.taskTitle,
-        description: this.taskDescription
+        description: this.taskDescription,
+        done: false
       });
       this.taskTitle = '';
       this.taskDescription = '';
     } else {
       alert('Please fill out both fields.');
     }
+  }
+
+  markAsDone(index: number) {
+    this.tasks[index].done = true;
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
   }
 }
